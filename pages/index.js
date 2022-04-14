afd6de1fbe55549b1f087f9fa778fb06de2ed544
Diana <dianac.salazarg@gmail.com>
2022-04-13 21:57:07 -0500

    se intenta filtrar por fechas recientes la pagina principal

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,37 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from "../components/layout";
+import React  from 'react';
+import Moment from 'react-moment';
 export default function Home({data}) {
-  return (
-     
+  const dateToFormat = new Date('1976-04-19T12:59-0500');
+            <Moment date={dateToFormat} />
+            console.log(dateToFormat)
+            var fechaInicio = new Date('2016-07-1').getTime();
+            var fechaFin    = new Date('2016-08-01').getTime();
+
+            var diff = fechaFin - fechaInicio;
+
+            console.log(diff/(24), 'differencia');
+  return (     
+    
     <Layout>
         {
-    data.map((post)=>(
-        <div key={post.id}>
-
-          <div className={styles.index}>
+          data.map((post)=>(
+            console.log(post),
+            <div class="container" className={styles.container}>
+              <h3>{post.title.rendered}</h3>
+              <div dangerouslySetInnerHTML={{__html: post.content.rendered}} />
+              <div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}} /> 
           
-            <div  className={styles.card}>
-            <h3>{post.title.rendered}</h3>
-            <div  dangerouslySetInnerHTML={{__html: post.content.rendered}} />
             </div>
-           
-          </div>
-         
-          </div>
-          
-
-    ))
-}
+          ))
+      }
         
     </Layout>
         
     )
 }
-
 export async function getStaticProps(){
     try{
 const res=await fetch('https://encasa.parqueexplora.org/wp-json/wp/v2/posts')
